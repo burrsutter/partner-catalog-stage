e2ebb05ba8b4e842010710898d555ed3ba687329
Burr Sutter <burrsutter@gmail.com>
2023-05-18 10:30:36 -0400

    Burr

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -41,8 +41,8 @@ public class DemoApplication {
       simpleDateFormat = new SimpleDateFormat(pattern);
       String stringNow = simpleDateFormat.format(new Date());
 
-      System.out.println("Aloha: " + cnt);
-      return "Aloha Spring " + cnt++ + " " + stringNow + " on " + HOSTNAME;
+      System.out.println("Burr: " + cnt);
+      return "Burr Spring " + cnt++ + " " + stringNow + " on " + HOSTNAME;
     }
 
     public static void main(String[] args) {
